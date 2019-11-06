e72e89c543979f366d06868f92ffe5bc9143098f
mlgc4869 <429342493@qq.com>
2019-11-07 02:28:33 +0800

    update test_image

--- linux/SDL/test_image.cpp
+++ linux/SDL/test_image.cpp
@@ -3,7 +3,11 @@
 #include "SDL.h"
 #include "SDL_image.h"
 
-int main() {
+int main(int argc, char * argv[]) {
+    if(argc<2) {
+        std::cout << "Usage: ./test_image <file>" << std::endl;
+	return -1;
+    }
     printf("InitializingSDL.\n");
     if((SDL_Init(SDL_INIT_VIDEO)==-1))//判断是否进入SDL库
     {
@@ -12,10 +16,10 @@ int main() {
     }
 
     printf("SDLinitialized.\n");
-    SDL_Surface*screen = NULL; // 创建一个窗口，并加载一张图片
-    screen =SDL_SetVideoMode( 642, 482, 24, SDL_SWSURFACE ); //创建SDL执行窗口并设置像素点及位深
+    SDL_Surface*screen = NULL; // 创建一个窗口，并加载一张图片
+    screen =SDL_SetVideoMode( 360, 640, 24, SDL_SWSURFACE ); //创建SDL执行窗口并设置像素点及位深
     SDL_Surface*img = NULL;
-    img =SDL_LoadBMP( "muq.bmp" ); //装载位图
+    img =SDL_LoadBMP( argv[1] ); //装载位图
     SDL_BlitSurface(img, NULL, screen, NULL ); //块移图面
     SDL_Flip(screen );//显示加载的图片
     SDL_UpdateRect(screen, 0, 0, 0, 0);
